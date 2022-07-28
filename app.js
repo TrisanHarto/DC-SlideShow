66b3d5ccc9a2c3fd3f9ff32b713d9eafcd6521e5
Howard Haryanto <hh@Howards-MacBook-Pro.local>
2022-07-28 18:52:15 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let posters = ['poster1.png','poster2.png','poster3.png','poster4.png','poster5.png'];
+let i =0;
+function slideShow() {
+    document.getElementById("poster").src=posters[i];
+    if(i<posters.length-1){
+        i++;
+    }
+    else {
+        i=0;
+    }
+
+    setTimeout("slideShow()" , 2000);
+}
+window.onload = slideShow;
